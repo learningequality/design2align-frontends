a4ca51074465afd5424fef46436dfc4613187a10
Richard Tibbles <richard@learningequality.org>
2019-10-15 12:55:52 -0700

    Add devserver message and catch login errors.

--- src/Login.vue
+++ src/Login.vue
@@ -7,7 +7,15 @@
     <input type="password" name="password" v-model="password" />
     <br />
     <button @click="login">Login</button>
+    <span v-if="error" style="color: red; font-weight: bold;">
+      There was an error logging in:
+      {{ error }}
+    </span>
     <p>Need an account? <a :href="registerLink">Register here</a></p>
+    <p v-if="devServer" style="color: red; font-weight: bold;">
+      Registration will redirect you to the production server, make sure to
+      reopen the devserver URL after registration
+    </p>
   </div>
 </template>
 
@@ -19,19 +27,28 @@ export default {
   data() {
     return {
       username: "",
-      password: ""
+      password: "",
+      error: null
     };
   },
   computed: {
     registerLink() {
       return `${baseUrl}/register/`;
+    },
+    devServer() {
+      return process.env.NODE_ENV !== "production";
     }
   },
   methods: {
     login() {
-      return login(this.username, this.password).then(() => {
-        this.$router.push({ name: "home" });
-      });
+      this.error = null;
+      return login(this.username, this.password)
+        .then(() => {
+          this.$router.push({ name: "home" });
+        })
+        .catch(err => {
+          this.error = err;
+        });
     }
   }
 };
