82f7b1c24ffa9dbbd7fc392cb4ff280e372ce732
Jordan Yoshihara <jordanyoshihara@gmail.com>
2019-10-17 11:16:18 -0700

    Updated lists to v-lists

--- src/apps/judgment/evaluation/Node.vue
+++ src/apps/judgment/evaluation/Node.vue
@@ -1,38 +1,48 @@
 <template>
-  <div>
-    <div>
-      <template v-if="nodeData.ancestors && nodeData.ancestors.length">
+  <v-container>
+    <template v-if="nodeData.ancestors && nodeData.ancestors.length">
+      <v-container fluid>
         <h3>Curricular context</h3>
-        <ul>
-          <li
+        <v-list>
+          <v-list-tile
             v-for="(nodeDatum, i) in nodeData.ancestors"
-            :key="'descendant' + nodeDatum.id"
+            :key="'ancestor' + nodeDatum.id"
             :style="{
-              paddingLeft: `${i * 5}px`,
-              marginLeft: `${i * 5}px`
+              paddingLeft: `${i * 10}px`,
+              marginLeft: `${i * 10}px`
             }"
           >
-            {{ nodeDatum.title }}
-          </li>
-        </ul>
-        <hr />
-      </template>
+            <v-list-tile-content>
+              <v-list-tile-title>
+                • &nbsp;{{ nodeDatum.title }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-container>
+      <v-divider />
+    </template>
+    <v-container fluid>
       <h3>Title</h3>
       <h2>{{ nodeData.title }}</h2>
-      <template v-if="nodeData.children && nodeData.children.length">
-        <hr />
+    </v-container>
+    <template v-if="nodeData.children && nodeData.children.length">
+      <v-divider />
+      <v-container fluid>
         <h3>Contents</h3>
-        <ul>
-          <li
+        <v-list>
+          <v-list-tile
             v-for="nodeDatum in nodeData.children"
             :key="'descendant' + nodeDatum.id"
           >
-            {{ nodeDatum.title }}
-          </li>
-        </ul>
-      </template>
-    </div>
-  </div>
+            <v-list-tile-content>
+              <v-list-tile-title> • {{ nodeDatum.title }} </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-container>
+    </template>
+  </v-container>
 </template>
 
 <script>
