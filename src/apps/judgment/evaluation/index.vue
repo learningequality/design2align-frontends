4e97c66f6c9c8a2eec7810dcc4543bc9327d1bcb
Jordan Yoshihara <jordanyoshihara@gmail.com>
2019-10-17 11:15:25 -0700

    Switched judgement questions to be under v-form

--- src/apps/judgment/evaluation/index.vue
+++ src/apps/judgment/evaluation/index.vue
@@ -1,41 +1,50 @@
 <template>
-  <v-layout row wrap>
-    <v-flex xs12>
-      <v-container fluid>
-        <Node v-if="node1" class="node subsection" :nodeData="node1" />
-        <Node v-if="node2" class="node subsection" :nodeData="node2" />
-      </v-container>
-    </v-flex>
-    <v-flex xs12>
-      <v-container fluid>
-        <v-form @submit="submitRating">
-          <v-radio-group
-            v-model="rating"
-            label="How related are these curriculum nodes?"
+  <v-container fluid>
+    <v-layout row wrap>
+      <v-flex xs6>
+        <Node v-if="node1" :nodeData="node1" />
+      </v-flex>
+      <v-flex xs6>
+        <Node v-if="node2" :nodeData="node2" />
+      </v-flex>
+      <v-flex xs12>
+        <v-container fluid>
+          <v-form
+            @submit.prevent="submitRating"
+            v-model="valid"
+            ref="form"
+            lazy-validation
           >
-            <v-radio label="Exact match" value="1" />
-            <v-radio label="Partial match" value="0.5" />
-            <v-radio label="Unrelated" value="0" />
-          </v-radio-group>
-          <v-radio-group
-            v-model="confidence"
-            label="How confident are you of this decision?"
-          >
-            <v-radio label="Completely" value="1" />
-            <v-radio label="Somewhat" value="0.5" />
-            <v-radio label="Not at all" value="0" />
-          </v-radio-group>
-          <v-textarea
-            label="What criteria did you use to make this decision?"
-            auto-grow
-          />
-          <v-btn :disabled="rating === null" type="submit">
-            Submit
-          </v-btn>
-        </v-form>
-      </v-container>
-    </v-flex>
-  </v-layout>
+            <v-radio-group
+              v-model="rating"
+              label="How related are these curriculum nodes?"
+              required
+              :rules="matchRules"
+            >
+              <v-radio label="Exact match" value="1" />
+              <v-radio label="Partial match" value="0.5" />
+              <v-radio label="Unrelated" value="0" />
+            </v-radio-group>
+            <v-radio-group
+              v-model="confidence"
+              label="How confident are you of this decision?"
+            >
+              <v-radio label="Completely" value="1" />
+              <v-radio label="Somewhat" value="0.5" />
+              <v-radio label="Not at all" value="0" />
+            </v-radio-group>
+            <v-textarea
+              label="What criteria did you use to make this decision?"
+              auto-grow
+            />
+            <v-btn type="submit">
+              Submit
+            </v-btn>
+          </v-form>
+        </v-container>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -52,11 +61,17 @@ export default {
       node2: null,
       rating: null,
       confidence: null,
-      comment: ""
+      comment: "",
+      valid: true
     };
   },
   created() {
     this.setNodes();
+  },
+  computed: {
+    matchRules() {
+      return [v => !!v || "This field is required"];
+    }
   },
   methods: {
     setNodes() {
@@ -71,25 +86,28 @@ export default {
       });
     },
     submitRating() {
-      const name = this.$route.name.replace("judgment-", "");
-      const uiName =
-        name.slice(0, 1).toUpperCase() + name.replace("-index", "").slice(1);
-      this.stopTimer();
-      return judgmentResource
-        .submitJudgment(
-          this.node1.id,
-          this.node2.id,
-          this.rating,
-          this.confidence,
-          uiName,
-          {
-            comment: this.comment,
-            time_for_judgment: this.elapsedTime
-          }
-        )
-        .then(() => {
-          return this.setNodes();
-        });
+      if (this.$refs.form.validate()) {
+        this.$refs.form.resetValidation();
+        const name = this.$route.name.replace("judgment-", "");
+        const uiName =
+          name.slice(0, 1).toUpperCase() + name.replace("-index", "").slice(1);
+        this.stopTimer();
+        return judgmentResource
+          .submitJudgment(
+            this.node1.id,
+            this.node2.id,
+            this.rating,
+            this.confidence,
+            uiName,
+            {
+              comment: this.comment,
+              time_for_judgment: this.elapsedTime
+            }
+          )
+          .then(() => {
+            return this.setNodes();
+          });
+      }
     },
     startTimer() {
       this.elapsedTime = null;
@@ -113,24 +131,4 @@ export default {
 };
 </script>
 
-<style lang="scss">
-.section {
-  display: inline-block;
-  width: 100%;
-  > :first-child {
-    float: left;
-  }
-  > :last-child {
-    float: right;
-  }
-}
-
-.subsection {
-  display: inline-block;
-  width: 49%;
-}
-
-.node {
-  border: red solid 1px;
-}
-</style>
+<style lang="scss"></style>
